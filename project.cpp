ee44485d3df6e098b30929f510238088edc2c41f
nathellman <49491410+nathellman@users.noreply.github.com>
2019-04-17 22:46:07 -0600

    Working functions
    Fixed bugs in print inventory function(s) and deleteAll function

--- project.cpp
+++ project.cpp
@@ -6,10 +6,30 @@
 #include "project.hpp"
 
 using namespace std;
+void addToList(LLNode *prev, LLNode * NewNode);
+void printTreeNode(TreeNode * travel);
+void printLLNode(LLNode * head);
 
 ArtMuseum::ArtMuseum()
 {
   root = NULL;
+}
+
+void DeleteAll(TreeNode * node)
+{
+	if (node)
+  {
+    DeleteAll(node -> leftChild);
+    LLNode * temp = node -> head;
+    while(temp != nullptr)
+    {
+      LLNode * next = temp -> next;
+      delete temp;
+      temp = next;
+    }
+    DeleteAll(node -> rightChild);
+    delete node;
+  }
 }
 
 ArtMuseum::~ArtMuseum()
@@ -18,80 +38,71 @@ ArtMuseum::~ArtMuseum()
   root = NULL;
 }
 
-void ArtMuseum::DeleteAll(TreeNode * node)
+void printLLNode(LLNode * head)
 {
-	if(node -> leftChild) DeleteAll(node -> leftChild);
-	if(node -> rightChild) DeleteAll(node -> rightChild);
-
-	LLNode * current = node -> head;
-	LLNode * temp = current;
-
-	while(current)
+  LLNode * travel = new LLNode;
+  travel = head;
+  if (travel == nullptr) cout << "Nothing in list" << endl;
+  while (travel != nullptr)
   {
-		temp = current;
-		cout << "Deleting: "<< current -> title << endl;
-		delete current;
+    cout << " >> " << travel->title << " " << travel->lastName << endl;
+    travel = travel->next;
+  }
+}
 
-		if(temp -> next) current = temp -> next;
-		else
-			break;
-	}
+void printTreeNode(TreeNode * travel)
+{
+  if (travel)
+  {
+    printTreeNode(travel->leftChild);
+    cout << "Artist's last name starting with the letter " << travel->nameChar << endl;
+    printLLNode(travel->head);
+    printTreeNode(travel->rightChild);
+  }
 }
 
 void ArtMuseum::printInventory()
 {
-  printInventory(root);
+  printTreeNode(root);
 }
 
-void ArtMuseum::printInventory(TreeNode * node)
+void ArtMuseum::addPiece(std::string firstName, std::string lastName, std::string region, std::string date, std::string medium, std::string title, std::string description, int galleryNumber, int yearCreated)
 {
-  if (node -> leftChild) printMovieInventory(node -> leftChild);
-
-  LLNode * current = node -> head;
-
-  while(current)
-  {
-    cout<< "Piece:  "<< current -> title << " Artist's last name: " << current -> lastName << endl;
-    current = current -> next;
-  }
-  if(node -> rightChild) printMovieInventory(node -> rightChild);
-}
-
-
-void addPeice(std::string firstName, std::string lastName, std::string region, std::string date, std::string medium, std::string title, std::string description, int galleryNumber, int yearCreated)
-{
-	LLNode * newPeice = new LLNode;
-	newPeice->firstName = firstName;
-	newPeice->lastName = lastName;
-	newPeice->region = region;
-	newPeice->date = date;
-	newPeice->medium = medium;
-	newPeice->title = title;
-	newPeice->description = description;
-	newPeice->galleryNumber = galleryNumber;
-	newPeice->yearCreated = yearCreated;
+	LLNode * newPiece = new LLNode;
+	newPiece->firstName = firstName;
+	newPiece->lastName = lastName;
+	newPiece->region = region;
+	newPiece->date = date;
+	newPiece->medium = medium;
+	newPiece->title = title;
+	newPiece->description = description;
+	newPiece->galleryNumber = galleryNumber;
+	newPiece->yearCreated = yearCreated;
 
 	TreeNode * newNode = new TreeNode;
 	newNode->nameChar = lastName.at(0);
 	newNode->leftChild = NULL;
 	newNode->rightChild = NULL;
 
-	//case 1 nothing in tree 
-	if(root == NULL){
+	//case 1 nothing in tree
+	if(root == NULL)
+  {
 		root = newNode;
 		newNode->parent = NULL;
-		newNode->head = newMovie;
-		newPeice->next = NULL;
+		newNode->head = newPiece;
+		newPiece->next = NULL;
 	}
-	else{
+	else
+  {
 		TreeNode * temp1 = root;
-		TreeNode * current - NULL;
 		int flag = 0;
-		while(temp1 && flag == 0){
-			current = temp1;
+		while(temp1 && flag == 0)
+    {
+			TreeNode * current = temp1;
 			//first node in linked list
-			if(newNode->nameChar == temp1->nameChar){
-				addToList(temp1->head, newPeice);
+			if(newNode->nameChar == temp1->nameChar)
+      {
+				addToList(temp1->head, newPiece);
 				flag = 1;
 			}
 			//traverse tree
@@ -102,12 +113,12 @@ void addPeice(std::string firstName, std::string lastName, std::string region, s
 
 			if(newNode->nameChar < current->nameChar && flag == 0){
 				current->leftChild = newNode;
-				current->leftChild->head = newPeice;
+				current->leftChild->head = newPiece;
 			}
 
 			if(newNode->nameChar > current->nameChar && flag == 0){
 				current->rightChild = newNode;
-				current->rightChild->head = newPeice;
+				current->rightChild->head = newPiece;
 			}
 
 			//sortTree(&current->head);
@@ -115,75 +126,11 @@ void addPeice(std::string firstName, std::string lastName, std::string region, s
 	}
 }
 
-
-void addToList(LLNode *prev, LLNode * NewNode){
+void addToList(LLNode *prev, LLNode * NewNode)
+{
   LLNode * tempNode = new LLNode;
   tempNode = NewNode;
   tempNode->next = prev->next;
   prev->next = tempNode;
   tempNode = NULL;
-}
-
-void sortTree(LLNode ** p)
-{
-	LLNode * tempNode = *p;
-	*p = NULL;
-	while(tempNode)
-	{
-		LLNode ** left = &tempNode;
-		LLNode ** right = &tempNode->next;
-		bool sorted = false;
-		while(*right)
-		{
-			if((*right)->lastName < (*left)->lastName)
-			{
-				std::swap(*left, *right);
-				std::swap((*left)->next, (*right)->next);
-				left = &(*left)->next;
-				sorted = true;
-			}
-			else
-			{
-				left = right;
-				right = &(*right)->next;
-			}
-		}
-	}
-}
-
-void searchArtist(std::firstName, std::lastName, LLNode * head)
-{
-	LLNode * temp = new LLNode;
-	temp = head;
-	while(temp)
-	{
-		if(temp->firstName == firstName && temp->lastName == lastName)
-			cout<<temp->title<<endl;
-		else
-			temp = temp->next;
-	}
-}
-
-
-void searchPeice(std::string title, LLNode * head)
-{
-	LLNode * temp = new LLNode;
-	temp = head; 
-	while(temp)
-	{
-		if(temp->title == title){
-			cout<<temp->title<<endl;
-			cout<<temp->firstName<<endl;
-			cout<<temp->lastName<<endl;
-			cout<<temp->region<<" ";
-			cout<<temp->date<<endl;
-			cout<<temp->yearCreated;
-			cout<<temp->medium<<endl;
-			cout<<temp->description<<endl;
-			cout<<temp->galleryNumber<<endl;
-		}
-		else
-			temp = temp->next;
-
-	}
 }
